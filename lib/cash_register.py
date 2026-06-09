e1c682871320feeff2a989f5856db7418029a0ff
muurucosmas <cosmasmuuuru988@gmail.com>
2026-06-09 15:19:03 +0300

    implemented how to add discount and staff

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,49 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+        self.discount = discount
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, value):
+        if not isinstance(value, int) or not (0 <= value <= 100):
+            print("Not valid discount")
+            self._discount = 0
+        else:
+            self._discount = value
+
+    def add_item(self, item, price, quantity):
+        self.total += price * quantity
+        self.items.append(item)
+
+        self.previous_transactions.append({
+            "item": item,
+            "price": price,
+            "quantity": quantity
+        })
+
+    def apply_discount(self):
+        if len(self.previous_transactions) == 0:
+            print("There is no discount to apply.")
+            return
+
+        self.total = self.total - (self.total * self.discount / 100)
+
+        self.previous_transactions.pop()
+
+    def void_last_transaction(self):
+        if not self.previous_transactions:
+            print("No transaction to void.")
+            return
+
+        last = self.previous_transactions.pop()
+
+        self.total -= last["price"] * last["quantity"]
+        self.items.pop()
